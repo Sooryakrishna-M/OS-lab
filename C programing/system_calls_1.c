023deaef19478fa86ce9e6173ee39e5737f7db4a
Sooryakrishna M <sk2004international@gmail.com>
2025-04-06 23:42:54 +0530

    commited the first c lang file

--- C programing/system_calls_1.c
+++ C programing/system_calls_1.c
@@ -1,20 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<direct.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
 
-struct dirent*dptr;
-int main (int argc,char*argv[]){
-char buff[100]
-DIR*dirp;
-printf("\n\nEnter the Directory Name");
-scanf("%s",buff);
-if((dirp=opendir(buff))==NULL){
-    printf("The given directory doesnt exist");
-    exit(1);
+struct dirent *dptr;
 
-}
-while(dptr=readdir(dirp)){
-    printf("%s\n",dprt->d_name);
-}
-closedir(dirp);
+int main(int argc, char *argv[]) {
+    char buff[100];
+    DIR *dirp;
+
+    printf("\n\nEnter the Directory Name: ");
+    scanf("%s", buff);
+
+    if ((dirp = opendir(buff)) == NULL) {
+        printf("The given directory doesn't exist.\n");
+        exit(1);
+    }
+
+    while ((dptr = readdir(dirp)) != NULL) {
+        printf("%s\n", dptr->d_name);
+    }
+
+    closedir(dirp);
+    return 0;
 }
