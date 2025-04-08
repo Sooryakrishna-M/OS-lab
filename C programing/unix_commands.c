4b74f0534e8311df95a599c6779b56e8b5dd2b29
Sooryakrishna M <sk2004international@gmail.com>
2025-04-08 09:26:09 +0530

    ls command are being studied

---
+++ C programing/unix_commands.c
@@ -0,0 +1,15 @@
+/*#include<stdio.h>
+#include<stdlib.h>
+#include<dirent.h>
+
+int main (int argc,char**argv){
+    DIR *dp;
+    struct dirent*link;
+    dp=opendir(arge[1]);
+    printf("\n contents of the directory %s are \n",argv[1]);
+    while((link==readdir(dp))!=0){
+        printf("%s",link->d_name);
+    }
+    closedir(dp);
+    return(0);
+}*/
